8ff6793c375969965b3ea3437e7ab3a00e7e2a28
ztucker4 <ztucker@hmc.edu>
2021-01-02 10:39:25 -0800

    icons now at center of mouse for placement on map

--- map.py
+++ map.py
@@ -278,7 +278,7 @@ class Map: #pylint: disable=too-many-instance-attributes
             if self.selected_inventory_item is not None: #selected defense follows mouse
                 selected_item_icon_x = marker.markers[self.selected_inventory_item].icon_coords[0]
                 selected_item_icon_y = marker.markers[self.selected_inventory_item].icon_coords[1]
-                pyxel.blt(pyxel.mouse_x, pyxel.mouse_y, 0, selected_item_icon_x, selected_item_icon_y, ICON_WIDTH, ICON_HEIGHT,0)
+                pyxel.blt(pyxel.mouse_x-8, pyxel.mouse_y-8, 0, selected_item_icon_x, selected_item_icon_y, ICON_WIDTH, ICON_HEIGHT,0)
             
             #show directions
             if self.show_directions is True:
